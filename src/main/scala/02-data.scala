1b3fc759971e154d8eb5d4af1d4f7e5594c2115e
sniggel <grmontpetit@gmail.com>
2021-02-20 17:03:41 -0500

    Added solution for 02.

--- src/main/scala/02-data.scala
+++ src/main/scala/02-data.scala
@@ -15,21 +15,26 @@
  * Enums and case classes provide first-class support for "algebraic data types" 
  * in Scala 3.
  */
-package enums: 
+package enums
+
+: 
   /**
    * EXERCISE 1
    * 
    * Convert this "sealed trait" to an enum.
    */
-  sealed trait DayOfWeek
-  object DayOfWeek:
-    case object Sunday extends DayOfWeek
-    case object Monday extends DayOfWeek
-    case object Tuesday extends DayOfWeek
-    case object Wednesday extends DayOfWeek
-    case object Thursday extends DayOfWeek
-    case object Friday extends DayOfWeek
-    case object Saturday extends DayOfWeek
+//  sealed trait DayOfWeek
+  enum DayOfWeek:
+    case Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+  end DayOfWeek
+//  object DayOfWeek:
+//    case object Sunday extends DayOfWeek
+//    case object Monday extends DayOfWeek
+//    case object Tuesday extends DayOfWeek
+//    case object Wednesday extends DayOfWeek
+//    case object Thursday extends DayOfWeek
+//    case object Friday extends DayOfWeek
+//    case object Saturday extends DayOfWeek
 
   /**
    * EXERCISE 2
@@ -37,8 +42,8 @@ package enums:
    * Explore interop with Java enums by finding all values of `DayOfWeek`, and by 
    * finding the value corresponding to the string "Sunday".
    */
-  def daysOfWeek: Array[DayOfWeek] = ???
-  def sunday: DayOfWeek = ???
+  def daysOfWeek: Array[DayOfWeek] = DayOfWeek.values
+  def sunday: DayOfWeek = DayOfWeek.valueOf("Sunday")
 
   /**
    * EXERCISE 3
@@ -47,12 +52,19 @@ package enums:
    * 
    * Take special note of the inferred type of any of the case constructors!
    */
-  sealed trait Color 
-  object Color:
-    case object Red extends Color 
-    case object Green extends Color 
-    case object Blue extends Color
-    final case class Custom(red: Int, green: Int, blue: Int) extends Color
+//  sealed trait Color 
+//  object Color:
+//    case object Red extends Color 
+//    case object Green extends Color 
+//    case object Blue extends Color
+//    final case class Custom(red: Int, green: Int, blue: Int) extends Color
+
+  enum Color(red: Int, green: Int, blue: Int):
+    case Custom(red: Int, green: Int, blue: Int) extends Color(red, green, blue)
+    case Red extends Color(255, 0, 0)
+    case Green extends Color(0, 255, 0)
+    case Blue extends Color(0, 0, 255)
+  end Color
 
   /**
    * EXERCISE 4
@@ -61,10 +73,15 @@ package enums:
    * 
    * Take special note of the inferred type parameters in the case constructors!
    */
-  sealed trait Result[+Error, +Value]
-  object Result:
-    final case class Succeed[Value](value: Value) extends Result[Nothing, Value]
-    final case class Fail[Error](error: Error) extends Result[Error, Nothing]
+//  sealed trait Result[+Error, +Value]
+//  object Result:
+//    final case class Succeed[Value](value: Value) extends Result[Nothing, Value]
+//    final case class Fail[Error](error: Error) extends Result[Error, Nothing]
+
+  enum Result[+Error, +Value](error: Error, value: Value):
+    case Succeed(error: Nothing, value: Value) extends Result(error, value)
+    case Failure(error: Error, value: Nothing) extends Result(error, value)
+  end Result
 
   /**
    * EXERCISE 5
@@ -73,19 +90,28 @@ package enums:
    * 
    * Take special note of the inferred type parameters in the case constructors!
    */
-  sealed trait Workflow[-Input, +Output]
-  object Workflow:
-    final case class End[Output](value: Output) extends Workflow[Any, Output]
+//  sealed trait Workflow[-Input, +Output]
+//  object Workflow:
+//    final case class End[Output](value: Output) extends Workflow[Any, Output]
+  enum Workflow[-Input, +Output] extends (Input => Output):
+    case End[I, O](f: I => O) extends Workflow[I, O]
+    final def apply(input: Input): Output = this match
+      case end: End[i, o] => end.f(input)
+  end Workflow
 
   /**
    * EXERCISE 6
    * 
    * Convert this "sealed trait" to an enum.
    */
-  sealed trait Conversion[-From, +To]
-  object Conversion:
-    case object AnyToString extends Conversion[Any, String]
-    case object StringToInt extends Conversion[String, Option[Int]]
+//  sealed trait Conversion[-From, +To]
+//  object Conversion:
+//    case object AnyToString extends Conversion[Any, String]
+//    case object StringToInt extends Conversion[String, Option[Int]]
+  enum Conversion[-From, +To]:
+    case AnyToString(from: Any) extends Conversion[Any, String]
+    case StringToInt(from: String) extends Conversion[String, Option[Int]]
+  end Conversion
 
 /**
  * CASE CLASSES
@@ -99,11 +125,12 @@ package case_classes:
    * By making the public constructor private, make a smart constructor for `Email` so that only 
    * valid emails may be created.
    */
-  final case class Email(value: String)
+  final case class Email private (value: String)
   object Email:
-    def fromString(v: String): Option[Email] = ???
+    def fromString(v: String): Option[Email] = if (isValidEmail(v)) Some(Email(v)) else None
 
     def isValidEmail(v: String): Boolean = v.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")
+  end Email
 
   /**
    * EXERCISE 2
@@ -111,7 +138,9 @@ package case_classes:
    * Try to make a copy of an existing `Email` using `Email#copy` and note what happens.
    * 
    */
-  def changeEmail(email: Email): Email = ???
+  def changeEmail(email: Email): Email = ???// email.copy("ffff@dddd.com")
+  // I cannot create a copy: method copy cannot be accessed as a member of (email : case_classes.Email) from module
+  // class 02-data$package$.
 
   /**
    * EXERCISE 3
@@ -119,7 +148,8 @@ package case_classes:
    * Try to create an Email directly by using the generated constructor in the companion object.
    * 
    */
-  def caseClassApply(value: String): Email = ???
+  def caseClassApply(value: String): Email = ??? //Email.fromString(value)
+  // The constructor fromString returns an Option, not an Email
 
 /**
  * PATTERN MATCHING
--- src/main/scala/welcome.scala
+++ src/main/scala/welcome.scala
@@ -1,3 +1,6 @@
+import enums._
+import case_classes._
+
 def assertTypesEqual[A, B >: A <: A]: Unit = ()
 
 object welcome:
@@ -16,3 +19,34 @@ object welcome:
   //    BREAK  :  2:00 PM London Time
   //    RESUME :  3:00 PM London Time
   //    END    :  7:00 PM London Time
+  def main(args: Array[String]): Unit = {
+    // DaysOfWeek
+    val dsow: Array[DayOfWeek] = daysOfWeek
+    val dow = sunday
+    println(dow)
+
+    // Colors
+    val customColor = Color.Custom(255, 255, 0)
+    println(customColor)
+    
+    // Workflow
+    val wf: String = Workflow.End(s => s.toString).apply(999)
+    println(wf)
+    
+    // Conversion
+    val cv = Conversion.AnyToString
+    println(cv)
+    
+    // Email
+    val email = Email.fromString("")
+    println(email)
+    val validEmail = Email.fromString("ABC0123@DEF456.COM")
+    println(validEmail)
+
+    // This doesn't work :method copy cannot be accessed as a member of (email : case_classes.Email) from module class
+    // 02-data$package$.
+//    val changedEmail = changeEmail(validEmail.get)
+//    print(changedEmail)
+
+    val newEmail = caseClassApply("hello")
+  }
